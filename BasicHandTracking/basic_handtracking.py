3d492c5e973762e3469204cda54a9757dea1457b
yassine khiari <yassine.khiari@outlook.com>
2022-04-19 15:04:48 +0200

    hand tracking with mediapipe

--- BasicHandTracking/basic_handtracking.py
+++ BasicHandTracking/basic_handtracking.py
@@ -14,10 +14,11 @@ while True:
     curent_time = time.time()
     fps = int(numer_of_frames / (curent_time - start_time))
     cv2.putText(frame, str(fps), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 2)
+    hand_detector.detect_hands_landmarks(frame)
     # frame = hand_detector.draw_landmarks_on_frame(frame, draw_connections=True)
     # hand_detector.draw_specific_landmarks_on_frame(frame, ids=range(1,21))
-    # ret_list = hand_detector.get_lm_coordinates_for_given_hand(frame, lm_id=4)
-    # print(ret_list)
+    ret_list = hand_detector.get_lm_coordinates_for_given_hand(frame, lm_id=4)
+    print(ret_list)
     cv2.imshow("image", frame)
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
